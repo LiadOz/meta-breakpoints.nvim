50b4e3c1a0572fe9309b18f50fe9005a011e4006
Liad Oz <liadozil@gmail.com>
2022-10-24 21:50:55 +0300

    changes

--- lua/meta-breakpoints.lua
+++ lua/meta-breakpoints.lua
@@ -28,13 +28,14 @@ function M.register_to_hook(hook_name, hook_function)
     end
 end
 
-M.register_to_hook('debug_hook', function() print('hook debug_hook called') end)
+--M.register_to_hook('debug_hook', function() print('hook debug_hook called') end)
 
 
 function M.toggle_meta_breakpoint(hook_name)
     local bufnr = vim.api.nvim_get_current_buf()
     local lnum = vim.api.nvim_win_get_cursor(0)[1]
     if M.remove_meta_breakpoint(bufnr, lnum) then
+        dap.toggle_breakpoint({}, {}, {})
         return
     end
     local sign_id = vim.fn.sign_place(
@@ -45,6 +46,7 @@ function M.toggle_meta_breakpoint(hook_name)
         { lnum = lnum; priority = 12 }
     )
     local bp = {
+        type = "continue",
         bufnr = bufnr,
         lnum = lnum,
         hook_name = hook_name,
@@ -60,25 +62,28 @@ function M.toggle_hook_breakpoint(hook_name)
     if M.remove_meta_breakpoint(bufnr, lnum) then
         return
     end
-    local sign_id = vim.fn.sign_place(
-        0,
-        sign_group,
-        'HookBreakpoint',
-        bufnr,
-        { lnum = lnum; priority = 12 }
-    )
+    --local sign_id = vim.fn.sign_place(
+        --0,
+        --sign_group,
+        --'HookBreakpoint',
+        --bufnr,
+        --{ lnum = lnum; priority = 12 }
+    --)
     local bp = {
         bufnr = bufnr,
         lnum = lnum,
         hook_name = "",
-        sign_id = sign_id,
+        --sign_id = sign_id,
     }
     table.insert(meta_breakpoints, bp)
+    print('inserted hook breakpoint')
     local function place_breakpoint()
         for _, win in ipairs(vim.api.nvim_list_wins()) do
             if vim.api.nvim_win_get_buf(win) == bufnr then
+                print('activating breakpoint')
                 vim.api.nvim_win_set_cursor(win, {lnum, 1})
-                dap.toggle_breakpoint({}, {"1"}, {})
+                dap.toggle_breakpoint({}, "1", {})
+                vim.cmd('sleep 5') -- instead of sleep you should listen to the add breakpoint hook and continue when it's done
             end
         end
     end
@@ -90,6 +95,7 @@ function M.remove_meta_breakpoint(bufnr, lnum)
         if bp.bufnr == bufnr and bp.lnum == lnum then
             vim.fn.sign_unplace(sign_group, { buffer = bufnr, id = bp.sign_id })
             table.remove(meta_breakpoints, i)
+            print('removed breakpoint')
             return true
         end
     end
@@ -109,7 +115,10 @@ local function continue_meta_breakpoint()
             for _, func in ipairs(hooks) do
                 func()
             end
-            dap.continue()
+            if bp.type == 'continue' then
+                print('before continue')
+                dap.continue()
+            end
         end
     end
 end
@@ -118,7 +127,7 @@ dap.listeners.after.stackTrace['meta-breakpoints.continue_meta'] = continue_meta
 
 function M.setup()
     vim.fn.sign_define('MetaBreakpoint', { text = "M", texthl = "", linehl = "", numhl = "" })
-    vim.fn.sign_define('HookBreakpoint', { text = "H", texthl = "", linehl = "", numhl = "" })
+    --vim.fn.sign_define('HookBreakpoint', { text = "H", texthl = "", linehl = "", numhl = "" })
 end
 M.setup()
 
