2087b3ec27f4c188aced5fbd83562ea759d91295
loz <loz@infinidat.com>
2022-10-25 16:40:37 +0300

    hook breakpoint works, removed prints

--- lua/meta-breakpoints.lua
+++ lua/meta-breakpoints.lua
@@ -10,15 +10,6 @@ local dap = require('dap')
 --local breakpoints = require('dap.breakpoints')
 --local Session = require('dap.session')
 
---local prev_event = Session.event_stopped
---function Session:event_stopped(stopped)
-    --print("inside the wrapper")
-    --print("calling the wrapped")
-    --prev_event(stopped)
-    --print("finished the wrapped")
---end
---print(vim.inspect(Session.event_stopped))
---Session.event_stopped = new_event
 
 function M.register_to_hook(hook_name, hook_function)
     if hooks_mapping.hook_name then
@@ -62,18 +53,19 @@ function M.toggle_hook_breakpoint(hook_name)
     if M.remove_meta_breakpoint(bufnr, lnum) then
         return
     end
-    --local sign_id = vim.fn.sign_place(
-        --0,
-        --sign_group,
-        --'HookBreakpoint',
-        --bufnr,
-        --{ lnum = lnum; priority = 12 }
-    --)
+    local sign_id = vim.fn.sign_place(
+        0,
+        sign_group,
+        'HookBreakpoint',
+        bufnr,
+        { lnum = lnum; priority = 12 }
+    )
     local bp = {
+        type = 'hook_activated',
         bufnr = bufnr,
         lnum = lnum,
         hook_name = "",
-        --sign_id = sign_id,
+        sign_id = sign_id,
     }
     table.insert(meta_breakpoints, bp)
     print('inserted hook breakpoint')
@@ -83,7 +75,6 @@ function M.toggle_hook_breakpoint(hook_name)
                 print('activating breakpoint')
                 vim.api.nvim_win_set_cursor(win, {lnum, 1})
                 dap.toggle_breakpoint({}, "1", {})
-                vim.cmd('sleep 5') -- instead of sleep you should listen to the add breakpoint hook and continue when it's done
             end
         end
     end
@@ -104,31 +95,34 @@ end
 
 local function continue_meta_breakpoint()
     local bufnr = vim.api.nvim_get_current_buf()
-    --print('bufnr=' .. bufnr)
     local lnum = vim.api.nvim_win_get_cursor(0)[1]
-    --print('lnum=' .. lnum)
     for _, bp in ipairs(meta_breakpoints) do
-        --print('comparing with ' .. vim.inspect(bp))
         if (bp.bufnr == bufnr and bp.lnum == lnum) then
-            --print('continuing')
             local hooks = hooks_mapping[bp.hook_name] or {}
             for _, func in ipairs(hooks) do
                 func()
             end
             if bp.type == 'continue' then
-                print('before continue')
+                --print('before continue')
                 dap.continue()
+            elseif bp.type == 'hook_activated' then
+                dap.toggle_breakpoint()
             end
         end
     end
 end
 
+--local function log_breakpoint(session, event)
+    --print("breakpoint event " .. event)
+--end
+--dap.listeners.after.breakpointEvent['meta-breakpoints.log_breakpoint'] = log_breakpoint
+
 dap.listeners.after.stackTrace['meta-breakpoints.continue_meta'] = continue_meta_breakpoint
+
 
 function M.setup()
     vim.fn.sign_define('MetaBreakpoint', { text = "M", texthl = "", linehl = "", numhl = "" })
-    --vim.fn.sign_define('HookBreakpoint', { text = "H", texthl = "", linehl = "", numhl = "" })
+    vim.fn.sign_define('HookBreakpoint', { text = "H", texthl = "", linehl = "", numhl = "" })
 end
-M.setup()
 
 return M
