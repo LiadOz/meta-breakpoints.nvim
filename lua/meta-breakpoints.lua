d7d9d144aada536190e6dcd2c38cb75bf1f9469b
Liad Oz <liadozil@gmail.com>
2022-11-27 21:48:30 +0200

    reload persistent breakpoints on file open

--- lua/meta-breakpoints.lua
+++ lua/meta-breakpoints.lua
@@ -6,21 +6,23 @@ local persistent = require('meta-breakpoints.persistent_bp')
 M.toggle_meta_breakpoint = breakpoints.toggle_meta_breakpoint
 M.simple_meta_breakpoint = breakpoints.simple_meta_breakpoint
 M.toggle_hook_breakpoint = breakpoints.toggle_hook_breakpoint
-M.load_persistent_breakpoints = breakpoints.load_persistent_breakpoints
 
 
 local function setup_opts(opts)
     opts.meta_breakpoint_sign = opts.meta_breakpoint_sign or 'M'
     opts.hook_breakpoint_sign = opts.hook_breakpoint_sign or 'H'
+    opts.allow_persistent_breakpoints = opts or true
 end
 
 function M.setup(opts)
     setup_opts(opts)
-    breakpoints.load_persistent_breakpoints()
-    vim.api.nvim_create_autocmd({'BufWritePost'}, {
-        pattern = {"*"},
-        callback = function() persistent.update_curr_buf_breakpoints(true) end
-    })
+    if opts.allow_persistent_breakpoints then
+        breakpoints.load_persistent_breakpoints()
+        vim.api.nvim_create_autocmd({'BufWritePost'}, {
+            pattern = {"*"},
+            callback = function() persistent.update_curr_buf_breakpoints(true) end
+        })
+    end
     vim.fn.sign_define('MetaBreakpoint', { text = opts.meta_breakpoint_sign, texthl = "", linehl = "", numhl = "" })
     vim.fn.sign_define('HookBreakpoint', { text = opts.hook_breakpoint_sign, texthl = "", linehl = "", numhl = "" })
 end
